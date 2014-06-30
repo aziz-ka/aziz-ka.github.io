175c18cf4e8890b150e872d12fb6e456ace48ccb
aziz-ka <hasanov.aziz@gmail.com>
2014-06-30 09:14:21 -0400

    some basic styling is done

---
+++ javascripts/scripts.js
@@ -0,0 +1,9 @@
+//$(function() {
+//	$("window").on("scroll", function() {
+//		console.log("hi");
+//	});
+//	$(".bullets li").on("click", function() {
+////		$("section").each(function(el) {})
+//		$(".project2").css("display", "block");
+//	})
+//});
